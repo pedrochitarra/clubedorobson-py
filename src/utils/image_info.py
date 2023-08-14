c1f4f85368f814c47faf0be41ee48737fd4c8306
Pedro Chitarra <pedrochitarra@hotmail.com>
2023-08-14 17:32:50 -0300

    [IMAGES] Dealing with external images

--- src/utils/image_info.py
+++ src/utils/image_info.py
@@ -1,5 +1,6 @@
 """Functions to get information about images."""
 import base64
+import requests
 
 
 def create_blob_from_file(file_path: str):
@@ -7,9 +8,14 @@ def create_blob_from_file(file_path: str):
     image tag to show an image from a file path. So, we need to convert the
     image to a blob and then use the src attribute of the image tag to show
     the image."""
-    file = open(file_path, "rb")
-    contents = file.read()
+    # Check if the image is local or remote
+    if file_path.startswith("http"):
+        response = requests.get(file_path)
+        contents = response.content
+    else:
+        file = open(file_path, "rb")
+        contents = file.read()
+        file.close()
     file_blob = base64.b64encode(contents).decode("utf-8")
-    file.close()
 
     return file_blob
