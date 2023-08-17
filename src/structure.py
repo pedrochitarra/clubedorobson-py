4586b8995509c01b39879da5f5006dd4d2b00768
Pedro Chitarra <pedrochitarra@hotmail.com>
2023-08-16 23:39:52 -0300

    [CODE] Removed unused script structure.py

--- src/structure.py
+++ src/structure.py
@@ -3,15 +3,6 @@ import pandas as pd
 import streamlit as st
 
 
-database_file = "data/raw/clubedorobson.db"
-connection = sqlite3.connect(database_file)
-cursor = connection.cursor()
-players = cursor.execute(
-    f"""UPDATE Jogadores SET proName = 'Show' WHERE proName = 'Sins'""")
-connection.commit()
-cursor.close()
-connection.close()
-
 class Match():
     def __init__(self, home_club: Club, away_club: Club) -> None:
         self.home_club = home_club
